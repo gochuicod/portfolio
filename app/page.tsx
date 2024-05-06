305671ce92bae929c0fe4d8fa09e2638bdf7bd1a
gochuicod <gochuicodarelleq@gmail.com>
2024-05-06 22:34:16 +0800

    Fixed scrolling issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,45 @@
 "use client"
 
 import Hero from "@/components/Hero";
-import { useEffect } from "react";
 import ReactPageScroller from "react-page-scroller";
+import { useEffect, useState } from 'react';
 
 const Home = () => {
-  let isMobile: boolean = false
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    isMobile = window.innerWidth <= 768
-  }, [])
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-    !isMobile ?
-    <ReactPageScroller>
-      <Hero/>
-      <div className="w-screen h-screen flex items-center justify-center text-8xl">2</div>
-      <div className="w-screen h-screen flex items-center justify-center text-8xl">3</div>
-      <div className="w-screen h-screen flex items-center justify-center text-8xl">4</div>
-    </ReactPageScroller>
-    :
-    <div>
-      <Hero/>
-      <div className="w-screen h-screen flex items-center justify-center text-8xl">2</div>
-      <div className="w-screen h-screen flex items-center justify-center text-8xl">3</div>
-      <div className="w-screen h-screen flex items-center justify-center text-8xl">4</div>
-    </div>
-  )
+    <>
+      {!isMobile && (
+        <ReactPageScroller>
+          <Hero/>
+          <div className="bg-teal-200 w-screen h-screen flex items-center justify-center text-8xl">2</div>
+          <div className="bg-cyan-200 w-screen h-screen flex items-center justify-center text-8xl">3</div>
+          <div className="bg-fuchsia-200 w-screen h-screen flex items-center justify-center text-8xl">4</div>
+        </ReactPageScroller>
+      )}
+      {isMobile && (
+        <div>
+          <Hero/>
+          <div className="bg-teal-200 w-screen h-screen flex items-center justify-center text-8xl">2</div>
+          <div className="bg-cyan-200 w-screen h-screen flex items-center justify-center text-8xl">3</div>
+          <div className="bg-fuchsia-200 w-screen h-screen flex items-center justify-center text-8xl">4</div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Home;
